4421fce6f3b803159e7fa05c28e6218668e3ff79
gjylimekaj <gm39212@ubt-uni.net>
2019-07-10 12:11:38 +0200

    .

--- contactus.php
+++ contactus.php
@@ -1,9 +1,26 @@
-<?php include 'includes/header.php'; ?>
 
+<?php include 'includes/header.php' ?>;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Planthea</title>
+    <link rel="stylesheet" href="css/contactus-style.css">
+        <link rel="stylesheet" href="css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $(".btn").click(function(){
+               $("input[type='text'], textarea").val(""); 
+            });              
+        });
+    </script>
+</head>
+<body>
     <div class="wrapper">
-        <div class = "content contactus">
+        <div class = "content">
             <h1>CONTACT FORM</h1>
-            <p>Connect with us by sendin your views.</p>
+            <p>Connect with us by sendind your views.</p>
         </div>
         <div class = "form">
             <div class = "top-form">
@@ -28,13 +45,13 @@
             </div>
             <div class = "bottom-form">
                 <div class = "inner-form">
-                    <div class = "label">Your message</div>
+                    <div class = "label">Message</div>
                     <textarea placeholder="Write something to us!"></textarea>
                 </div>
             </div>
             <div class= "btn">send form</div>
         </div>
     </div>
-
-</body>
+    
+</body>    
 </html>
